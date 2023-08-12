c4b9843371fc9a16177fb37b7e8a3b7ebc6f0c3a
w3villa-shubham-rai <shubham.rai@w3villa.com>
2023-08-12 17:04:01 +0530

    loader function implement

--- Asset/js/index.js
+++ Asset/js/index.js
@@ -1,3 +1,19 @@
+window.onload = function () {
+    var loader = document.getElementById("loader");
+    var container = document.getElementById("container1");
+
+    if (loader && container) {
+        
+        loader.style.display = "block";
+        container.style.display = "none";
+
+        setTimeout(function () {
+            loader.style.display = "none"; 
+            container.style.display = "flex"; 
+        }, 3000);
+    }
+};
+
 $('#slider').owlCarousel({
     loop:true,
     margin:10,
@@ -7,3 +23,5 @@ $('#slider').owlCarousel({
     dots:true,
 
 })
+
+
